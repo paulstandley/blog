a4a95918710ee818e248ec3ad1809a25ffb7bc35
Paul Standley <paulstandley1972@gmail.com>
2019-07-20 16:38:24 +0100

    remove map

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -11,30 +11,7 @@ export default class Index extends Component {
             form: true
          };
     }
-    componentWillMount() {
-         /* I wish I had come up with this but Elharony did he did great so simple */
-    function renderScripElement(srcUrl) {
-    /* make sure its the first script tag make a script tag add atrrs */  
-      const firstChildScriptTag = window.document.getElementsByTagName('script')[0];
-      //const secondChildScriptTag = window.document.getElementsByTagName('script')[1];
-      const scriptTag = window.document.createElement('script');
-      //const scriptTag1 = window.document.createElement('script');
-      scriptTag.src = srcUrl;
-      scriptTag.defer = true;  
-      scriptTag.async = true;
-      /*const name = `
-      ${function gm_authFailure() {
-        window.alert("Google Maps error!");
-      }}`;*/
-      
-    /* then place it in the dom as a first child */ 
-      firstChildScriptTag.parentNode.insertBefore(scriptTag, firstChildScriptTag);
-      //secondChildScriptTag.parentNode.insertBefore(scriptTag1, secondChildScriptTag);
-      //secondChildScriptTag.parentNode.insertBefore(inside, secondChildScriptTag);
-    }
-    /* call render scrip and pass in google map url and api key */
-    renderScripElement('https://maps.googleapis.com/maps/api/js?key=AIzaSyDcheCgHTyf9zr3vcCCSOo0wrq_W95sUcA&callback=initMap');
-    }
+
     render() {
         return (
             <div>

76139b8de5a8e8da9ec3620a24bbe9a1d9d5b783
Paul Standley <paulstandley1972@gmail.com>
2019-07-21 18:52:17 +0100

    content for about and landing pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,4 +4,8 @@
     <header class="jumbotron text-center">
       <h1><strong>{{$title}}</strong></h1>
     </header>
+    <div class="card  text-center">
+      <div class="card-header"><h2>You will need to signin to make a post</h2></div>
+      <div class="card-body"><h3>Select a title add your blog to the body and optional add image</h3></div> 
+    </div>
 @endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,4 +4,9 @@
     <header class="jumbotron text-center">
       <h1><strong>{{$title}}<strong></h1>
     </header>
+    <div class="card  text-center">
+      <div class="card-header"><h2>Made by <a href="http://paulstandley.com">Paul Standley</a></h2></div>
+      <div class="card-body"><h3>This is a Laravel Blade PHP MYSQL React javaScript CSS3 Bootstrap4 SCSS Blog App</h3></div> 
+      <div class="card-footer">Thanks to <a href="http://https://www.youtube.com/user/TechGuyWeb" target="_blank">Traversy Media</a></div>
+    </div>
 @endsection
